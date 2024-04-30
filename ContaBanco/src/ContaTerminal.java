543aad67d0d2773eb36cc38043c014374db7950d
ViniciusCris <143042392+Vini14cius09@users.noreply.github.com>
2024-04-29 22:25:02 -0300

    Finalização do projeto "Simulando uma conta bancária"

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.*;
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o número da sua agencia: ");
+        String agencia = scanner.next();
+
+        System.out.print("Digite o número da sua conta: ");
+        int conta = scanner.nextInt();
+
+        System.out.print("Digite o seu nome: ");
+        String nomeDoCliente = scanner.next();
+
+        System.out.print("Digite o seu saldo em conta: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeDoCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque");
+
+        scanner.close();
     }
 }
